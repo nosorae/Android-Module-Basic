909a5d07d43fc5a35872d35163c59f3910c01dc8
nosorae <nosorae@naver.com>
2021-07-15 21:37:59 +0900

    Initial commit

---
+++ app/src/main/java/nosorae/module_basic/MainActivity.kt
@@ -0,0 +1,28 @@
+package nosorae.module_basic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+// 그래들은 안드로이드 앱을 빌드하는데 도움을 주는 것
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+/*
+Android 파일탐색기
+크게 manifest, java, res로 나뉨
+manifest는 앱의 특징, 기능, 구조, 권한 명시
+java에는 소스코드들이 들어있음
+res에는 앱에서 사용하는 이미지 소리 영상 폰트 레이아웃 같은 코드외의 모든 것이 들어간다.
+drawable, layout, mipmap, values...
+drawable 이미지 리소스 저장
+layout 앱이 어떻게 그려질지 저장하는 도화지역할
+mipmap 이미지 리소스 저장
+values 여러 값들을 저장해서 하드코딩을 방지?
+
+Gradle Scripts
+각종 앱에관한 설정들
+ */
